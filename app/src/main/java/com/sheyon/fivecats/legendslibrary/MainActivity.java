a95524b3f5cc81ce2c4c37f04c9bbd91a62f1c85
sheyon <jessicagriggs@gmail.com>
2017-05-11 14:11:19 -0400

    Database Update

--- app/src/main/java/com/sheyon/fivecats/legendslibrary/MainActivity.java
+++ app/src/main/java/com/sheyon/fivecats/legendslibrary/MainActivity.java
@@ -1,18 +1,21 @@
 package com.sheyon.fivecats.legendslibrary;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ExpandableListView;
-import android.widget.ListView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.sheyon.fivecats.legendslibrary.data.LegendsContract;
 import com.sheyon.fivecats.legendslibrary.data.LegendsHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView legendsListView;
     private ExpandableListView legendsExpandableView;
     private Cursor cursor;
 
@@ -21,31 +24,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //legendsListView = (ListView) findViewById(R.id.legends_listView);
         legendsExpandableView = (ExpandableListView) findViewById(R.id.legends_expandable_list);
 
-        displayDatabaseInfo();
+        legendsExpandableView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
 
-        //OPTIONAL LATER
-        //set up Content Provider
-        //set up legendsCursorAdaptor
-        //set up legendsCursorLoader
-        //set up legendsListView.onItemClickListener
+                LinearLayout ll = (LinearLayout) v;
+                TextView tv = (TextView) ll.findViewById(R.id.subcategory_text_view);
+                String clickedSubcatText = tv.getText().toString();
+
+                Log.v("***STRING: ", "" + clickedSubcatText);
+                return false;
+            }
+        });
+
+        displayDatabaseInfo();
     }
 
     private void displayDatabaseInfo()
     {
-        if (cursor != null)
-        {
-            cursor.close();
-        }
-
         LegendsHelper legendsHelper = new LegendsHelper(this);
         SQLiteDatabase legendsDB = legendsHelper.getReadableDatabase();
 
         cursor = legendsDB.rawQuery(LegendsContract.CAT_QUERY, null, null);
 
-        LegendsCursorTreeAdaptor legendsCursorTreeAdaptor = new LegendsCursorTreeAdaptor(cursor, this);
+        LegendsCursorTreeAdapter legendsCursorTreeAdaptor = new LegendsCursorTreeAdapter(cursor, this);
         legendsExpandableView.setAdapter(legendsCursorTreeAdaptor);
 
         //TO UTILISE THE CURSOR ADAPTOR
