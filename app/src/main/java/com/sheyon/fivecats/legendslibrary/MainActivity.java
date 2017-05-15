50a8008a9002f2f64fd9883f608a2120e97fcb08
sheyon <jessicagriggs@gmail.com>
2017-05-14 20:05:11 -0400

    Basic spinner

--- app/src/main/java/com/sheyon/fivecats/legendslibrary/MainActivity.java
+++ app/src/main/java/com/sheyon/fivecats/legendslibrary/MainActivity.java
@@ -4,11 +4,15 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.sheyon.fivecats.legendslibrary.data.LegendsContract.Queries;
@@ -19,11 +23,15 @@ public class MainActivity extends AppCompatActivity {
     private SQLiteDatabase legendsDB;
     private ExpandableListView legendsExpandableView;
     private Cursor cursor;
+    private int spinnerSelection;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupSpinner();
 
         //OPEN DATABASE
         LegendsHelper legendsHelper = new LegendsHelper(this);
@@ -46,6 +54,27 @@ public class MainActivity extends AppCompatActivity {
         });
 
         displayCategoryScreen();
+    }
+
+    private void setupSpinner()
+    {
+        Spinner spinner = (Spinner) findViewById(R.id.category_spinner);
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this, R.array.categories_array, android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     private void displayCategoryScreen()
