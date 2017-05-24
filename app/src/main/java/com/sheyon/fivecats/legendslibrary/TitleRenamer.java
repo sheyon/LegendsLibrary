6f37fabbe79594622d61a90483ddbbf131729986
sheyon <jessicagriggs@gmail.com>
2017-05-24 18:03:55 -0400

    title renamer

--- app/src/main/java/com/sheyon/fivecats/legendslibrary/TitleRenamer.java
+++ app/src/main/java/com/sheyon/fivecats/legendslibrary/TitleRenamer.java
@@ -4,165 +4,162 @@ public class TitleRenamer
 {
     //THIS CUSTOM CLASS IS FOR NORMALIZING LORE TITLES FOR TOAST MESSAGES
     public String convertTitle (String title) {
-        String mTitle;
         String mRenamed;
 
-        mTitle = title;
-
-        if (mTitle.equals("Draculeşti, The")){
+        if (title.equals("Draculeşti, The")){
             mRenamed = "The Draculeşti";
             return mRenamed;
         }
-        if (mTitle.equals("Abandoned, The")){
+        if (title.equals("Abandoned, The")){
             mRenamed = "The Abandoned";
             return mRenamed;
         }
-        if (mTitle.equals("Ankh, The")){
+        if (title.equals("Ankh, The")){
             mRenamed = "The Ankh";
             return mRenamed;
         }
-        if (mTitle.equals("Black House, The")){
+        if (title.equals("Black House, The")){
             mRenamed = "The Black House";
             return mRenamed;
         }
-        if (mTitle.equals("Black Signal, The")){
+        if (title.equals("Black Signal, The")){
             mRenamed = "The Black Signal";
             return mRenamed;
         }
-        if (mTitle.equals("Bomb, The")){
+        if (title.equals("Bomb, The")){
             mRenamed = "The Bomb";
             return mRenamed;
         }
-        if (mTitle.equals("Buzzing, The")){
+        if (title.equals("Buzzing, The")){
             mRenamed = "The Buzzing";
             return mRenamed;
         }
-        if (mTitle.equals("Call of the Nameless, The")){
+        if (title.equals("Call of the Nameless, The")){
             mRenamed = "The Call of the Nameless";
             return mRenamed;
         }
-        if (mTitle.equals("Cargo, The")){
+        if (title.equals("Cargo, The")){
             mRenamed = "The Cargo";
             return mRenamed;
         }
-        if (mTitle.equals("Darkness War, The")){
+        if (title.equals("Darkness War, The")){
             mRenamed = "The Darkness War";
             return mRenamed;
         }
-        if (mTitle.equals("Dream Palace, The")){
+        if (title.equals("Dream Palace, The")){
             mRenamed = "The Dream Palace";
             return mRenamed;
         }
-        if (mTitle.equals("Filth, The")){
+        if (title.equals("Filth, The")){
             mRenamed = "The Filth";
             return mRenamed;
         }
-        if (mTitle.equals("Fog, The")){
+        if (title.equals("Fog, The")){
             mRenamed = "The Fog";
             return mRenamed;
         }
-        if (mTitle.equals("Franklin Mansion, The")){
+        if (title.equals("Franklin Mansion, The")){
             mRenamed = "The Franklin Mansion";
             return mRenamed;
         }
-        if (mTitle.equals("Gungne, The")){
+        if (title.equals("Gungne, The")){
             mRenamed = "The Gungne";
             return mRenamed;
         }
-        if (mTitle.equals("Hierarchy of Oni, The")){
+        if (title.equals("Hierarchy of Oni, The")){
             mRenamed = "The Hierarchy of Oni";
             return mRenamed;
         }
-        if (mTitle.equals("Host, The")){
+        if (title.equals("Host, The")){
             mRenamed = "The Host";
             return mRenamed;
         }
-        if (mTitle.equals("House-in-Exile, The")){
+        if (title.equals("House-in-Exile, The")){
             mRenamed = "The House-in-Exile";
             return mRenamed;
         }
-        if (mTitle.equals("Jingu Clan, The")){
+        if (title.equals("Jingu Clan, The")){
             mRenamed = "The Jingu Clan";
             return mRenamed;
         }
-        if (mTitle.equals("Jinn and the First Age, The")){
+        if (title.equals("Jinn and the First Age, The")){
             mRenamed = "The Jinn and the First Age";
             return mRenamed;
         }
-        if (mTitle.equals("Kingdom, The")){
+        if (title.equals("Kingdom, The")){
             mRenamed = "The Kingdom";
             return mRenamed;
         }
-        if (mTitle.equals("Korinto-kai, The")){
+        if (title.equals("Korinto-kai, The")){
             mRenamed = "The Korinto-kai";
             return mRenamed;
         }
-        if (mTitle.equals("Lady Margaret, The")){
+        if (title.equals("Lady Margaret, The")){
             mRenamed = "The Lady Margaret";
             return mRenamed;
         }
-        if (mTitle.equals("Maiden Shrine, The")){
+        if (title.equals("Maiden Shrine, The")){
             mRenamed = "The Maiden Shrine";
             return mRenamed;
         }
-        if (mTitle.equals("Manufactory, The")){
+        if (title.equals("Manufactory, The")){
             mRenamed = "The Manufactory";
             return mRenamed;
         }
-        if (mTitle.equals("Marya, The")){
+        if (title.equals("Marya, The")){
             mRenamed = "The Marya";
             return mRenamed;
         }
-        if (mTitle.equals("Mitsubashi, The")){
+        if (title.equals("Mitsubashi, The")){
             mRenamed = "The Mitsubashi";
             return mRenamed;
         }
-        if (mTitle.equals("Ofuda, The")){
+        if (title.equals("Ofuda, The")){
             mRenamed = "The Ofuda";
             return mRenamed;
         }
-        if (mTitle.equals("Orochi Group, The")){
+        if (title.equals("Orochi Group, The")){
             mRenamed = "The Orochi Group";
             return mRenamed;
         }
-        if (mTitle.equals("Polaris, The")){
+        if (title.equals("Polaris, The")){
             mRenamed = "The Polaris";
             return mRenamed;
         }
-        if (mTitle.equals("Pyramid, The")){
+        if (title.equals("Pyramid, The")){
             mRenamed = "The Pyramid";
             return mRenamed;
         }
-        if (mTitle.equals("Sentinels, The")){
+        if (title.equals("Sentinels, The")){
             mRenamed = "The Sentinels";
             return mRenamed;
         }
-        if (mTitle.equals("Third Age, The")){
+        if (title.equals("Third Age, The")){
             mRenamed = "The Third Age";
             return mRenamed;
         }
-        if (mTitle.equals("Tragical History of Doctor Faustus, The")){
+        if (title.equals("Tragical History of Doctor Faustus, The")){
             mRenamed = "The Tragical History of Doctor Faustus";
             return mRenamed;
         }
-        if (mTitle.equals("Truce, The")){
+        if (title.equals("Truce, The")){
             mRenamed = "The Truce";
             return mRenamed;
         }
-        if (mTitle.equals("Wall, The")){
+        if (title.equals("Wall, The")){
             mRenamed = "The Wall";
             return mRenamed;
         }
-        if (mTitle.equals("Whispering Tide, The")){
+        if (title.equals("Whispering Tide, The")){
             mRenamed = "The Whispering Tide";
             return mRenamed;
         }
-        if (mTitle.equals("Manna of Saint Nicholas, The")){
+        if (title.equals("Manna of Saint Nicholas, The")){
             mRenamed = "The Manna of Saint Nicholas";
             return mRenamed;
         }
         else {
-            mRenamed = mTitle;
+            mRenamed = title;
         }
         return mRenamed;
     }
