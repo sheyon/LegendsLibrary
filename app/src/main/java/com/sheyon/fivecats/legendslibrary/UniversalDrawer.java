10b0140a1e08857a19b478c2f8078a9261df3737
sheyon <jessicagriggs@gmail.com>
2017-05-31 23:38:07 -0400

    Drawer Beautify-ing

--- app/src/main/java/com/sheyon/fivecats/legendslibrary/UniversalDrawer.java
+++ app/src/main/java/com/sheyon/fivecats/legendslibrary/UniversalDrawer.java
@@ -18,14 +18,15 @@ public class UniversalDrawer extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
 
+
     public void setupDrawer(Activity activity, Toolbar toolbar){
         mActivity = activity;
         mDrawerLayout = (DrawerLayout) mActivity.findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) mActivity.findViewById(R.id.left_drawer);
-        String[] mDrawerItems = mActivity.getResources().getStringArray(R.array.drawer_items);
+        final String[] mDrawerItems = mActivity.getResources().getStringArray(R.array.drawer_items);
 
         // Set the adapter for the list view
-        ArrayAdapter<String> drawerAdapter = new ArrayAdapter<String>(mActivity, android.R.layout.simple_list_item_1, mDrawerItems);
+        DrawerAdapter drawerAdapter = new DrawerAdapter(mActivity, R.layout.drawer_list_item, mDrawerItems);
         mDrawerList.setAdapter(drawerAdapter);
         mDrawerToggle = new ActionBarDrawerToggle(mActivity, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close){
             public void onDrawerClosed(View view) {
@@ -40,14 +41,14 @@ public class UniversalDrawer extends AppCompatActivity {
         mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position == 0) {
+                if (mDrawerItems[position].equals("Legends Library")) {
                     Intent intent = new Intent(mActivity, MainActivity.class);
                     if (mActivity.getClass() != MainActivity.class) {
                         mActivity.startActivity(intent);
                         mDrawerLayout.closeDrawer(mDrawerList);
                     }
                 }
-                if (position == 1) {
+                if (mDrawerItems[position].equals("About")) {
                     Intent intent = new Intent(mActivity, AboutActivity.class);
                     if (mActivity.getClass() != AboutActivity.class) {
                         mActivity.startActivity(intent);
