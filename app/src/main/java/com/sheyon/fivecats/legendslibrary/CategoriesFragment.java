4144fa1479effdf5c2f34429ca9167f4ec60a428
sheyon <jessicagriggs@gmail.com>
2017-06-16 15:57:18 -0400

    French locale

--- app/src/main/java/com/sheyon/fivecats/legendslibrary/CategoriesFragment.java
+++ app/src/main/java/com/sheyon/fivecats/legendslibrary/CategoriesFragment.java
@@ -6,7 +6,6 @@ import android.database.sqlite.SQLiteQueryBuilder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
