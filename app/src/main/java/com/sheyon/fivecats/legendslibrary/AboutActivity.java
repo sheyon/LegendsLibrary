5a8ef2fcb3feb6c0237059b083d86cbce6c9ebf6
sheyon <jessicagriggs@gmail.com>
2017-08-16 20:17:09 -0400

    1.1.3b - Rotation Handler

--- app/src/main/java/com/sheyon/fivecats/legendslibrary/AboutActivity.java
+++ app/src/main/java/com/sheyon/fivecats/legendslibrary/AboutActivity.java
@@ -48,7 +48,7 @@ public class AboutActivity extends AppCompatActivity {
         universalDrawer.setupDrawer(this, toolbar);
 
         RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.aboutActivity_relativeLayout);
-        final ScrollView scrollView = (ScrollView) findViewById(R.id.aboutActivity_scrollView);
+        ScrollView scrollView = (ScrollView) findViewById(R.id.aboutActivity_scrollView);
         RotationHandler rotationHandler = new RotationHandler();
         rotationHandler.setupRotationLayout(this, relativeLayout, scrollView, toolbar);
 
